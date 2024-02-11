5297fe96722cbd76ad694238d72c2aeade37e8e6
Slava Osipov <skybright@mail.ru>
2024-02-11 14:53:59 +0300

    task_268841 refactoring data structures

--- src/main/java/ru/job4j/pooh/TopicSchema.java
+++ src/main/java/ru/job4j/pooh/TopicSchema.java
@@ -2,43 +2,42 @@ package ru.job4j.pooh;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class TopicSchema implements Schema {
-    private final ConcurrentHashMap<String, CopyOnWriteArrayList<Receiver>> receivers = new ConcurrentHashMap<>();
-    private final ConcurrentHashMap<String, BlockingQueue<String>> data = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, ConcurrentHashMap<Receiver, BlockingQueue<String>>> data = new ConcurrentHashMap<>();
     private final Condition condition = new Condition();
 
     @Override
     public void addReceiver(Receiver receiver) {
-        receivers.putIfAbsent(receiver.name(), new CopyOnWriteArrayList<>());
-        receivers.get(receiver.name()).add(receiver);
+        data.putIfAbsent(receiver.name(), new ConcurrentHashMap<>());
+        data.get(receiver.name()).putIfAbsent(receiver, new LinkedBlockingQueue<>());
         condition.on();
     }
 
     @Override
     public void publish(Message message) {
-        data.putIfAbsent(message.name(), new LinkedBlockingQueue<>());
-        data.get(message.name()).add(message.text());
+        data.putIfAbsent(message.name(), new ConcurrentHashMap<>());
+        var mapReceivers = data.get(message.name());
+        for (var queueEntry : mapReceivers.entrySet()) {
+            queueEntry.getValue().add(message.text());
+        }
         condition.on();
     }
 
     @Override
     public void run() {
         while (!Thread.currentThread().isInterrupted()) {
-            for (var queueKey : receivers.keySet()) {
-                var queue = data.getOrDefault(queueKey, new LinkedBlockingQueue<>());
-                var receiversByQueue = receivers.get(queueKey);
-                while (true) {
-                    var data = queue.poll();
-                    if (data != null) {
-                        for (Receiver receiver : receiversByQueue) {
-                            receiver.receive(data);
+            for (var receiversOnTopic : data.values()) {
+                for (var receiverWithQueue : receiversOnTopic.entrySet()) {
+                    while (true) {
+                        var data = receiverWithQueue.getValue().poll();
+                        if (data != null) {
+                            receiverWithQueue.getKey().receive(data);
                         }
-                    }
-                    if (data == null) {
-                        break;
+                        if (data == null) {
+                            break;
+                        }
                     }
                 }
             }
